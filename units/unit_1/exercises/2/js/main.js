03832c476d449ba95ae7bd7bd66226a1e6069597
Eddie Melendez <exmelendez@gmail.com>
2019-09-05 22:31:16 -0400

    added unit 1 exercise two

---
+++ units/unit_1/exercises/2/js/main.js
@@ -0,0 +1,9 @@
+const test1 = 60,
+  test2 = 75,
+  test3 = 80;
+
+const name = prompt("What is students name?");
+
+const average = test1 + test2 + test3 / 3;
+
+alert(`${name} has averaged a ${average} in math.`);
