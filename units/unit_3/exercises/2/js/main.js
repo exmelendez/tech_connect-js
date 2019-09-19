55a3507a03156e76674e0f8ab4b6e880b178a8df
Eddie Melendez <exmelendez@gmail.com>
2019-09-19 19:24:25 -0400

    added comments to unit 3 ex 2 js file

--- units/unit_3/exercises/2/js/main.js
+++ units/unit_3/exercises/2/js/main.js
@@ -1,10 +1,11 @@
 const convertToOunces = () => {
-  const input = document.querySelector("input");
-  const amountInLiters = input.value;
-  const pTag = document.querySelectorAll("p");
-  const ouncesDrank = amountInLiters * 33.814;
-  const cupsDrank = amountInLiters * 4.227;
+  const input = document.querySelector("input"); //HTML INPUT ELEMENT
+  const amountInLiters = input.value; // VALUE OF THE HTML INPUT ELEMENT UPON SUBMISSION
+  const pTag = document.querySelectorAll("p"); // THE ENTIRE COLLECTION OF P TAG HTML ELEMENTS
+  const ouncesDrank = amountInLiters * 33.814; // CALCULATION OF OUNCES BASED OFF INPUT VALUE
+  const cupsDrank = amountInLiters * 4.227; // CALCULATION OF CUPS BASED OFF INPUT VALUE
 
+  /* FOR LOOP USED TO ITERATE THROUGH THE COLLECTION OF HTML P TAG ELEMENTS. MAKES USE OF A CONDITIONAL TO OUTPUT TO THE FIRST OR THE SECOND P TAG ELEMENT. */
   for (let i = 0; i < pTag.length; i++) {
     i == 0
       ? (pTag[
@@ -17,6 +18,7 @@ const convertToOunces = () => {
         )} ounces of water.`);
   }
 
+  /* EXTRA CREDIT OUTPUT OF CUPS OF WATER */
   pTag[2].textContent = `That comes out to ${cupsDrank.toFixed(
     1
   )} cups of water.`;
@@ -24,4 +26,5 @@ const convertToOunces = () => {
   input.value = "";
 };
 
+/* CLICK LISTENER GRABBING THE FIRST BUTTON ELEMENT IN HTML FILE */
 document.querySelector("button").addEventListener("click", convertToOunces);
